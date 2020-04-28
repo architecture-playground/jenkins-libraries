51bf267cc71db3ddb384fcd07170e12d63bd03bf
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-04-28 16:30:58 +0300

    Add split checkBranchAndPushImage.groovy

--- vars/pushImageToRepository.groovy
+++ vars/pushImageToRepository.groovy
@@ -8,9 +8,13 @@ def call(body) {
     }
     echo "** Docker login finished"
 
+    String fname =env.JOB_MANE;
+    String split  = fname.split("\\");
+    System.out.println(" :: value " + split[0] );
+
     sh '''#!/bin/bash -ex
 echo "** Building application docker image started" && \\
-docker build --target app -t architectureplayground/feature-toggle:latest . && \\
+docker build --target app -t architectureplayground/+split[0]+:latest . && \\
 echo "** Building application docker image finished" && \\
 
 echo "** Start pushing docker image in docker hub repository" && \\
