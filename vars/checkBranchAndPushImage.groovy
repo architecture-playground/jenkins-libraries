6c91b4c6ff138388ac41a97b6533aaa923f7e161
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-04-28 12:39:14 +0300

    Add checkBranchAndPushImage.groovy

--- vars/checkBranchAndPushImage.groovy
+++ vars/checkBranchAndPushImage.groovy
@@ -1,19 +1,14 @@
 #!/usr/bin/env groovy
 
 def call(body) {
-    when {
-        expression {
-            return env.BRANCH_NAME == 'master';
-        }
-    }
-    {
-        echo "** Docker login started"
-        withCredentials([usernamePassword(credentialsId: 'dockerhub_architectureplayground', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh '''docker login -u $USERNAME -p $PASSWORD'''
-        }
-        echo "** Docker login finished"
 
-        sh '''#!/bin/bash -ex
+    echo "** Docker login started"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub_architectureplayground', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh '''docker login -u $USERNAME -p $PASSWORD'''
+    }
+    echo "** Docker login finished"
+
+    sh '''#!/bin/bash -ex
 echo "** Building application docker image started" && \\
 docker build --target app -t architectureplayground/feature-toggle:latest . && \\
 echo "** Building application docker image finished" && \\
@@ -22,5 +17,5 @@ echo "** Start pushing docker image in docker hub repository" && \\
 docker push architectureplayground/feature-toggle:latest && \\
 echo "** Docker image pushed to docker hub repository"
                     '''
-    }
 }
+
