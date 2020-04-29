643aa85bc3620d594f07b1fb5204867525e6b826
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-04-29 14:42:50 +0300

    add splitJobName.groovy

--- vars/pushImageToRepository.groovy
+++ vars/pushImageToRepository.groovy
@@ -2,7 +2,7 @@
 
 def call(body) {
 
-    echo ("** Docker login started")
+    echo("** Docker login started")
     withCredentials([usernamePassword(credentialsId: 'dockerhub_architectureplayground', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh '''docker login -u $USERNAME -p $PASSWORD'''
     }
@@ -11,7 +11,7 @@ def call(body) {
 
     repositoryName = env.JOB_NAME.split("/")[0]
 
-    sh '''#!/bin/bash -ex
+    sh """#!/bin/bash -ex
 echo "** Building application docker image started" && \\
 docker build --target app -t architectureplayground/${repositoryName}:latest . && \\
 echo "** Building application docker image finished" && \\
@@ -19,6 +19,6 @@ echo "** Building application docker image finished" && \\
 echo "** Start pushing docker image in docker hub repository" && \\
 docker push architectureplayground/${repositoryName}:latest && \\
 echo "** Docker image pushed to docker hub repository"
-                    '''
+                    """
 }
 
