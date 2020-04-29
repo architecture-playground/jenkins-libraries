90451ef3e50fd98b69d3e9d65b36c6da65269fae
BulgakovAnton <bylgakov.anton.a@gmailcom>
2020-04-29 14:41:02 +0300

    add reponame

--- vars/pushImageToRepository.groovy
+++ vars/pushImageToRepository.groovy
@@ -2,19 +2,22 @@
 
 def call(body) {
 
-    echo "** Docker login started"
+    echo ("** Docker login started")
     withCredentials([usernamePassword(credentialsId: 'dockerhub_architectureplayground', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh '''docker login -u $USERNAME -p $PASSWORD'''
     }
+
     echo "** Docker login finished"
+
+    repositoryName = env.JOB_NAME.split("/")[0]
 
     sh '''#!/bin/bash -ex
 echo "** Building application docker image started" && \\
-docker build --target app -t architectureplayground/"+env.JOB_NAME.split("/")[0]+":latest . && \\
+docker build --target app -t architectureplayground/${repositoryName}:latest . && \\
 echo "** Building application docker image finished" && \\
 
 echo "** Start pushing docker image in docker hub repository" && \\
-docker push architectureplayground/"+env.JOB_NAME.split("/")[0]+":latest && \\
+docker push architectureplayground/${repositoryName}:latest && \\
 echo "** Docker image pushed to docker hub repository"
                     '''
 }
